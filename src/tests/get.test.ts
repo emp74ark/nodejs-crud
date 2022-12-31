d94795d3de5078184ab2bf02152a8b6384195d3a
emp74ark <emp74ark@gmail.com>
2022-12-31 12:10:14 +0100

    refactor: get tests code reformat

--- src/tests/get.test.ts
+++ src/tests/get.test.ts
@@ -1,32 +1,32 @@
 import supertest from 'supertest';
 
-const host = '127.0.0.1'
-const port = '4000'
-const request = supertest(`${host}:${port}`)
+const host = '127.0.0.1';
+const port = '4000';
+const request = supertest(`${host}:${port}`);
 
 describe('GET requests', () => {
   it('greeting from root', async () => {
-    const result = await request.get('/')
-    expect(result.statusCode).toBe(200)
-    expect(result.text).toBe(`API address is ${host}:${port}/api`)
-  })
+    const result = await request.get('/');
+    expect(result.statusCode).toBe(200);
+    expect(result.text).toBe(`API address is ${host}:${port}/api`);
+  });
   it('greeting from api', async () => {
-    const result = await request.get('/api')
-    expect(result.statusCode).toBe(200)
-    expect(result.text).toBe('api')
-  })
+    const result = await request.get('/api');
+    expect(result.statusCode).toBe(200);
+    expect(result.text).toBe('api');
+  });
   it('get users', async () => {
-    const result = await request.get('/api/users/')
-    expect(result.statusCode).toBe(200)
-    expect(result.text).toBe('[]')
-  })
+    const result = await request.get('/api/users/');
+    expect(result.statusCode).toBe(200);
+    expect(result.text).toBe('[]');
+  });
   it('get 400', async () => {
-    const result = await request.get('/api/users/1')
-    expect(result.statusCode).toBe(400)
-  })
+    const result = await request.get('/api/users/1');
+    expect(result.statusCode).toBe(400);
+  });
   it('get 404', async () => {
-    const result = await request.get('/api/unknown')
-    expect(result.statusCode).toBe(404)
-    expect(result.text).toBe('Not found')
-  })
-})
+    const result = await request.get('/api/unknown');
+    expect(result.statusCode).toBe(404);
+    expect(result.text).toBe('Not found');
+  });
+});
